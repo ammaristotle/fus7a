d7294b1b9a85ce196392b48e8b31d9f3ff1fa049
Ammar Khan <adkhan@protonmail.com>
2018-08-20 23:34:00 -0400

    Make initial exercise UI ready
    Added navigation between questions
    Added a second sample question. TODO: hook up API
    
    TODO: Fix drawer and add navigation between exercises

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-
-import { Instructions, Question, AnswerChoices, Response } from './components/exercise';
+import { Instructions, Question, AnswerChoices, Response, Navigation } from './components/exercise';
 
 // const myWord = 'إنِّي أُحِبُّ اللهَ وَ رَسُولَهُ'.split(' ').reverse();
 
@@ -14,8 +13,9 @@ class Exercise extends React.Component {
       hint: '',
       activeExercise: 'ex-name',
       activeQuestion: 0,
-      activeAnswerChoiceResponse: '',
+      answerChoiceResponse: '',
       exerciseType: 'multiple_choice',
+      numOfQuestions: 2,
       questions: [
         {
           text: 'My teacher drinks chocolate milk regularly',
@@ -26,6 +26,16 @@ class Exercise extends React.Component {
           answerChoices: ['Raf', 'Nasb', 'Jarr'],
           answerChoiceResponses: ['That is not correct', 'That is correct', 'That is not correct'],
           correctAnswer: 1,
+        },
+        {
+          text: 'He loves chocolate milk',
+          translation: '',
+          lang: 'en',
+          bold: true,
+          boldIndex: 0,
+          answerChoices: ['Raf', 'Nasb', 'Jarr'],
+          answerChoiceResponses: ['That is correct', 'That is not correct', 'That is not correct'],
+          correctAnswer: 0,
         }
       ]
     }
@@ -36,26 +46,49 @@ class Exercise extends React.Component {
     // Set response in state
   }
 
+  /**
+   * Go forward and back between questions
+   * @param  {String} direction either "next" or not
+   * @return {Void}
+   */
   navigateBetweenQuestions(direction) {
-    // Hide previous answer response
-    if (direction === 'forward') {
-      // Change question
-    } else {
-      // Change question
+    // Decide whether to go to next or previous question
+    const increment = (direction === 'next' ? 1 : -1);
+    const { activeQuestion, numOfQuestions } = this.state;
+    // The question we want to end up at
+    const endingNumber = activeQuestion + increment;
+    // If we reached the end, we don't want to do anything
+    const reachedEndOfQuestions = (
+      (endingNumber === -1) || (endingNumber === numOfQuestions)
+      ? true
+      : false
+    );
+    // As long as we're not the end, navigate to next or previous question
+    if (!reachedEndOfQuestions) {
+      this.setState((prevState, props) => {
+        return {
+          activeQuestion: prevState.activeQuestion + increment,
+          answerChoiceResponse: '',
+        };
+      });
     }
   }
 
+  /**
+   * Handle user selected answer and display response
+   * @param  {Number} indexOfAnswerSelected taken from answerChoices in each
+                      question
+   * @return {Void}
+   */
   handleAnswerResponse(indexOfAnswerSelected) {
     const { questions, activeQuestion } = this.state;
     const currentQuestion = questions[activeQuestion];
-    const responseToSelectedAnswer = currentQuestion.answerChoiceResponses[indexOfAnswerSelected];
-    if (indexOfAnswerSelected === questions[activeQuestion].correctAnswer) {
-      this.setState({ correctAnswerSelected: true });
-      // handle correct behavior
-    } else {
-      this.setState({ correctAnswerSelected: false })
-    }
-    this.setState({ answerChoiceResponse: responseToSelectedAnswer });
+    const { correctAnswer, answerChoiceResponses } = currentQuestion;
+    const responseToSelectedAnswer = answerChoiceResponses[indexOfAnswerSelected];
+    this.setState({
+      answerChoiceResponse: responseToSelectedAnswer,
+      correctAnswerSelected: indexOfAnswerSelected === correctAnswer,
+    });
   }
 
   render() {
@@ -72,12 +105,13 @@ class Exercise extends React.Component {
     return (
       <div className="wrapper">
         <Instructions instructions={instructions} />
-        <Question questions={questions} active={0} />
+        <Question questions={questions} active={activeQuestion} />
         <AnswerChoices
           handle={this.handleAnswerResponse.bind(this)}
           choices={answerChoices}
         />
         { answerChoiceResponse && <Response text={answerChoiceResponse} correct={correctAnswerSelected} /> }
+        <Navigation handleChange={this.navigateBetweenQuestions.bind(this)} />
       </div>
     );
   }
