a45ceca59458004e94927036fb2c97d017709d96
Ammar Khan <adkhan@protonmail.com>
2018-08-30 03:51:30 -0400

    Add progress indicator for current question
    Now, when questions are being cycled through, the user will be able to view their progress (the current question they are on) in light of the total number of questions for the exercise

--- src/components/exercise/navigate/index.jsx
+++ src/components/exercise/navigate/index.jsx
@@ -1,29 +1,30 @@
-import React from  'react';
+import React, { PureComponent } from  'react';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
 import ArrowForward from '@material-ui/icons/ArrowForward';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 
 import './navigate.css'
 
-class Navigation extends React.PureComponent {
+class Navigation extends PureComponent {
 
   static propTypes = {
     handleChange: PropTypes.func.isRequired
   };
 
   render() {
-    const { handleChange } = this.props;
+    const { handleChange, current, total } = this.props;
     return (
       <div className="nav-buttons">
+        <div className="progress">Progress: { current + 1 }/{ total }</div>
         <Button variant="contained" className="left-button" color="primary" onClick={handleChange.bind(this, 'previous')}>
           <ArrowBack />Prev
         </Button>
         <Button variant="contained" color="primary" onClick={handleChange.bind(this, 'next')}>
           Next<ArrowForward />
         </Button>
+        <br />
+        Donate
       </div>
     )
   }
