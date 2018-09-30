b9122e16fc133eb28d7cfdc4f1c46c9f183733a4
Ammar Khan <adkhan@protonmail.com>
2018-09-30 17:47:53 -0400

    Do not display response text immediately
    When an answer is clicked, allow the user to expand to see the explanation

--- src/components/exercise/response/index.jsx
+++ src/components/exercise/response/index.jsx
@@ -10,11 +10,40 @@ class Response extends React.PureComponent {
     correct: PropTypes.bool,
   };
 
+  state = {
+    explain: false,
+  }
+
+  explain = () => {
+    this.setState((state) => {
+      return {
+        explain: !state.explain
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // De-expand explanation if response is different
+    if (nextProps.text !== this.props.text) this.setState({ explain: false });
+  }
+
+  // TODO
+  // Use random response words instead of correct or incorrect
   render() {
     const { text, correct } = this.props;
+    const { explain } = this.state;
     return (
-      <div className={`response ${correct ? 'correct' : 'incorrect'}`}>
-        { text }
+      <div
+        className={`response ${correct ? 'correct' : 'incorrect'}`}
+        onClick={this.explain}
+      >
+        <span>
+          <span style={{ fontWeight: 'bold' }}>
+            { correct ? 'Correct' : 'Incorrect'}
+          </span>
+          &nbsp;(click for explanation)
+        </span>
+        { explain && <div style={{ marginTop: '15px' }}>{ text} </div>}
       </div>
     )
   }
