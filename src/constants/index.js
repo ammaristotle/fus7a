0aae902d9a11df5d9734a32275449991d0ac5722
Ammar Khan <adkhan@protonmail.com>
2018-10-05 20:25:06 -0400

    Make lesson 2 available

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,23 +6,39 @@ export const exercises = [
       {
         text: "Ism, F'il and Harf",
         lesson: 'lesson1_part1',
-        new: true,
+        new: false,
       },
       {
         text: "Raf', Nasb, and Jarr",
         lesson: 'lesson1_part2',
-        new: true,
+        new: false,
       },
       {
         text: 'Sounds and Combinations',
         lesson: 'lesson1_part3',
-        new: true,
+        new: false,
       },
       {
         text: 'Status in Arabic',
         lesson: 'lesson1_part4',
-        new: true,
+        new: false,
       }
     ]
   },
+  {
+    text: 'Lesson 2',
+    nested: false,
+    nest: [
+      {
+        text: 'Light and Heavy',
+        lesson: 'lesson2_part1_v2',
+        new: true,
+      },
+      {
+        text: 'Flexibility',
+        lesson: 'lesson2_part2_v2',
+        new: true,
+      }
+    ]
+  }
 ];
